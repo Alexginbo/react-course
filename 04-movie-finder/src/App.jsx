f8c0a951d6294f3d78724f1210b656579a8faafc
alexandre.giner <alexandre.giner@inetum.com>
2023-11-30 15:34:34 +0100

    Movie Finder Project v0

--- 04-movie-finder/src/App.jsx
+++ 04-movie-finder/src/App.jsx
@@ -1,18 +1,33 @@
 import { useState } from 'react'
 import './App.css'
+import { Movies } from './components/movies'
+import { useMovies } from './hooks/useMovies'
+import { useRef } from 'react'
+
+//useRef es como un useState pero cada vez que cambia no se vuelve a renderizar el componente
+//también nos permite hacer bastantes cosas más
 
 function App() {
+  const {movies} = useMovies()
+  const inputRef = useRef()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const value = inputRef.current.value 
+    alert(value)
+  }
+  
   return (
     <div className='page'>
       <header>
         <h1>Buscador de pelis</h1>
-        <form action="">
-          <input placeholder='Avengers, Star Wars ...'/>
-          <button>Buscar</button>
+        <form action="" onSubmit={handleSubmit}>
+          <input ref={inputRef} placeholder='Avengers, Star Wars ...'/>
+          <button type='Submit'>Buscar</button>
         </form>
       </header>
       <main>
-        Aquí irán las pelis
+        <Movies movies={movies}/>      
      </main>
     </div>
   )
